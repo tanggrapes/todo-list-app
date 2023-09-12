01726a6b94f145bda9d5ddf4705b4a420bff870b
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 02:20:11 +0800

    feat: create signup implementation in UserService

---
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.marktoledo.todolistapi.service.impl;
+
+import com.marktoledo.todolistapi.domain.User;
+import com.marktoledo.todolistapi.dto.request.SignUpRequest;
+import com.marktoledo.todolistapi.dto.response.AuthenticationResponse;
+import com.marktoledo.todolistapi.dto.response.SignUpResponse;
+import com.marktoledo.todolistapi.exception.UsernameAlreadyExistException;
+import com.marktoledo.todolistapi.repository.UserRepository;
+import com.marktoledo.todolistapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Override
+    public SignUpResponse signUp(SignUpRequest signUpRequest) {
+//        check username
+        checkIfUsernameExist(signUpRequest.getUsername());
+        User user = User.builder()
+                .firstName(signUpRequest.getFirstName())
+                .lastName(signUpRequest.getLastName())
+                .username(signUpRequest.getUsername())
+                .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                .roles("ROLE_USER").build();
+        userRepository.save(user);
+        return SignUpResponse.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .build();
+    }
+
+    private void checkIfUsernameExist(String username){
+        User user = userRepository.getUserByUsername(username);
+
+        if(user == null){
+            throw new UsernameAlreadyExistException("Username already exist");
+        }
+
+    }
+}
