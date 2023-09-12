89aea0e6a0335cf2ba31b2a7ac099732629e9c6a
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 02:17:17 +0800

    feat: create customer exception for username that already exist

---
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/exception/UsernameAlreadyExistException.java
@@ -0,0 +1,12 @@
+package com.marktoledo.todolistapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class UsernameAlreadyExistException extends RuntimeException {
+
+    public UsernameAlreadyExistException(String message) {
+        super(message);
+    }
+}
