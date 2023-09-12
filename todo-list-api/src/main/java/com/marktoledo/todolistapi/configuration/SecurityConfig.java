22f44684514e71815be47d3e665529d47ea75e96
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 02:29:38 +0800

    feat: create SecurityConfig

---
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/configuration/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.marktoledo.todolistapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+
+}
