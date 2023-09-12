70e2be5944d473848b8eec6920b155704a9f907d
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 01:41:38 +0800

    chore: backend initial project setup

---
+++ todo-list-api/src/test/java/com/marktoledo/todolistapi/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marktoledo.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
