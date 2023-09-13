7b8f494808daf13485480dbc2f21c6df00c75d47
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 11:16:57 +0800

    feat: create AuthenticationResponse

---
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/dto/response/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.marktoledo.todolistapi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+}
