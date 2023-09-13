e4069d5c18c79fce071cb8b60202853cb53b95e3
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 16:40:09 +0800

    feat: modify SecurityConfig

--- todo-list-api/src/main/java/com/marktoledo/todolistapi/configuration/SecurityConfig.java
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/configuration/SecurityConfig.java
@@ -1,21 +1,47 @@
 package com.marktoledo.todolistapi.configuration;
 
+import com.marktoledo.todolistapi.filter.JwtAuthTokenFilter;
 import com.marktoledo.todolistapi.repository.UserRepository;
+import com.marktoledo.todolistapi.security.RestAuthenticationEntryPoint;
+import com.marktoledo.todolistapi.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+
+    private UserRepository userRepository;
+
+    private JwtAuthTokenFilter jwtAuthTokenFilter;
+
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(UserRepository userRepository,
+                          JwtAuthTokenFilter jwtAuthTokenFilter, RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
+        this.userRepository = userRepository;
+        this.jwtAuthTokenFilter = jwtAuthTokenFilter;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+    }
 
 
     @Bean
@@ -24,18 +50,40 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(userRepository);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/api/v1/users/sign-up").permitAll()
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/v1/users/sign-up",
+                                "/api/v1/users/sign-in").permitAll()
                         .anyRequest().authenticated()
                 )
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(restAuthenticationEntryPoint))
                 .build();
     }
-
 
 
 }
