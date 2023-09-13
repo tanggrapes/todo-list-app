7b049d6aa9153cc38dfc9b0f295cd0d842806b6f
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-13 16:22:51 +0800

    feat: add signIn method to UserService

--- todo-list-api/src/main/java/com/marktoledo/todolistapi/service/UserService.java
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/service/UserService.java
@@ -1,10 +1,14 @@
 package com.marktoledo.todolistapi.service;
 
+import com.marktoledo.todolistapi.dto.request.SignInRequest;
 import com.marktoledo.todolistapi.dto.request.SignUpRequest;
+import com.marktoledo.todolistapi.dto.response.AuthenticationResponse;
 import com.marktoledo.todolistapi.dto.response.SignUpResponse;
 
 public interface UserService {
 
     SignUpResponse signUp(SignUpRequest signUpRequest);
+
+    AuthenticationResponse signIn(SignInRequest signInRequest);
 
 }
