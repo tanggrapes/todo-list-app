cd9d6aff0333f14d4a8b4f5c9d0e29e1831f67b2
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-09-15 01:06:00 +0800

    feat: update todo service and implementation

--- todo-list-api/src/main/java/com/marktoledo/todolistapi/service/TodoService.java
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/service/TodoService.java
@@ -1,8 +1,10 @@
 package com.marktoledo.todolistapi.service;
 
 import com.marktoledo.todolistapi.dto.request.CreateTodoRequest;
+import com.marktoledo.todolistapi.dto.request.UpdateTodoRequest;
 import com.marktoledo.todolistapi.dto.response.CreateTodoResponse;
 import com.marktoledo.todolistapi.dto.response.TodoListResponse;
+import com.marktoledo.todolistapi.dto.response.UpdateTodoResponse;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +14,8 @@ public interface TodoService {
     CreateTodoResponse createTodo(CreateTodoRequest request, UUID userId);
 
     List<TodoListResponse> getTodoList(UUID userId);
+
+    UpdateTodoResponse updateTodo(UUID todoId, UpdateTodoRequest request);
 
 
 
--- todo-list-api/src/main/java/com/marktoledo/todolistapi/service/impl/TodoServiceImpl.java
+++ todo-list-api/src/main/java/com/marktoledo/todolistapi/service/impl/TodoServiceImpl.java
@@ -3,13 +3,17 @@ package com.marktoledo.todolistapi.service.impl;
 import com.marktoledo.todolistapi.domain.Todo;
 import com.marktoledo.todolistapi.domain.User;
 import com.marktoledo.todolistapi.dto.request.CreateTodoRequest;
+import com.marktoledo.todolistapi.dto.request.UpdateTodoRequest;
 import com.marktoledo.todolistapi.dto.response.CreateTodoResponse;
 import com.marktoledo.todolistapi.dto.response.TodoListResponse;
+import com.marktoledo.todolistapi.dto.response.UpdateTodoResponse;
 import com.marktoledo.todolistapi.repository.TodoRepository;
 import com.marktoledo.todolistapi.repository.UserRepository;
 import com.marktoledo.todolistapi.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,7 +33,8 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public CreateTodoResponse createTodo(CreateTodoRequest request, UUID userId) {
-        User user = userRepository.getReferenceById(userId);
+
+        User user = userRepository.findById(userId).get();
         Todo todo = Todo.builder()
                 .dueDate(request.getDueDate())
                 .title(request.getTitle())
@@ -42,20 +47,45 @@ public class TodoServiceImpl implements TodoService {
                 .dueDate(todo.getDueDate())
                 .title(todo.getTitle())
                 .description(todo.getDescription())
+                .createdAt(todo.getCreatedAt())
+                .updatedAt(todo.getUpdatedAt())
                 .build();
     }
 
     @Override
     public List<TodoListResponse> getTodoList(UUID userId) {
-        User user = userRepository.getReferenceById(userId);
+        User user = userRepository.findById(userId).get();
         return user.getTodos().stream()
                 .map(todo -> TodoListResponse.builder()
+                        .id(todo.getId())
                         .isCompleted(todo.getIsCompleted())
+                        .dueDate(todo.getDueDate())
                         .title(todo.getTitle())
                         .description(todo.getDescription())
-                        .dueDate(todo.getDueDate())
-                        .id(todo.getId()).build()
+                        .createdAt(todo.getCreatedAt())
+                        .updatedAt(todo.getUpdatedAt())
+                        .build()
                 ).toList();
     }
+
+    @Override
+    public UpdateTodoResponse updateTodo(UUID todoId, UpdateTodoRequest request) {
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not exist"));
+        todo.setTitle(request.getTitle());
+        todo.setDescription(request.getDescription());
+        todo.setDueDate(request.getDueDate());
+        todo.setIsCompleted(request.getIsCompleted());
+        todoRepository.save(todo);
+        return UpdateTodoResponse.builder()
+                .createdAt(todo.getCreatedAt())
+                .updatedAt(todo.getUpdatedAt())
+                .title(todo.getTitle())
+                .description(todo.getDescription())
+                .isCompleted(todo.getIsCompleted())
+                .dueDate(todo.getDueDate())
+                .build();
+    }
+
 
 }
